d326ce011b33b29eda2e5ac8069d28eab6a062d0
christinebautista96-max <christinebautista96@gmail.com>
2025-10-26 14:29:32 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,17 +11,8 @@ function clearDisplay() {
 function calculate() {
   try {
     const result = eval(display.value);
-    addToHistory(display.value + ' = ' + result);
     display.value = result;
-  } catch {
+  } catch (error) {
     display.value = 'Error';
   }
 }
-
-function addToHistory(entry) {
-  const item = document.createElement('div');
-  item.className = 'history-item';
-  item.textContent = entry;
-  historyList.prepend(item);
-}
-
